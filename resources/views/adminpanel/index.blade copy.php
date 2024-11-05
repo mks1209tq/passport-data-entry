b9f52cef9909b5b7723fd62ab3ed44aa5bf34f04
Mohammed Maroof <19404@TQ.INV>
2024-11-05 11:21:24 +0400

    3 column layout in admin panel

--- resources/views/adminpanel/index.blade copy.php
+++ resources/views/adminpanel/index.blade copy.php
@@ -8,80 +8,16 @@
     </x-slot>
 
     @section('content')
-
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
-<div class="flex min-h-full flex-col border">
-
-
-  <!-- 3 column wrapper -->
-  <div class="mx-auto w-full max-w-7xl grow lg:flex xl:px-1">
-    <!-- Left sidebar & main wrapper -->
-    <div class="flex-2 xl:flex">
-      <div class="border-b px-1 py-2 sm:px-1 lg:pl-1 xl:w-96 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-2">
-        <!-- Left column area -->
-        <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-2 lg:px-3">
+    
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-gray-900 flex flex-col">
-                    <div class="px-1">
-                        <!-- Passport per User Count -->
-                        @php
-                            $passportsPerUser = $passports->count() / $users->count();
-                        @endphp 
-                        <table class="border border-slate-500">
-                            <th class="border border-slate-500 text-left p-2">User</th>
-                            <th class="border border-slate-500 text-center p-2">Assigned</th>
-                            <th class="border border-slate-500 text-center p-2">Entered</th>
-                        @foreach ($users as $user)
-                        <tr>
-                                <td class="border border-slate-500 p-2">
-                            
-                                    {{ $user->name }}
-                                </td>
-                                <td class="border border-slate-500 text-center p-2">
-                                    {{ $passports->where('user_id', $user->id)->count() }}
-                                </td>
-                                <td class="border border-slate-500 text-center p-2">
-                                    {{ $passports->where('user_id', $user->id)
-                                    ->where('is_data_entered', true)
-                                    ->count() }}
-                                
-                            </td>
-                        </tr>
-                        @endforeach
-                        </table>
-                    </div>
-
-                </div>
-            </div>
-            </div>
-        </div>
-      </div>
-      <!-- end of passport per user count -->
-
-        
-
-      </div>
-
-      <div class="px-1 py-2 sm:px-2 lg:pl-3 xl:flex-1 xl:pl-3">
-        <!-- Main area -->
-         <!-- register,verifier,assign passports -->
-        <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-1 lg:px-2">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-gray-900">
+                <div class="p-6 text-gray-900">
                     <!-- {{ __("Welcome! ") }} -->
                        <!-- Flash Messages -->
                        @if (session('success'))
-                        <div class="pt-1">
-                            <div class="max-w-7xl mx-auto sm:px-1 lg:px-2 xl:px-0">
+                        <div class="pt-3">
+                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 xl:px-0">
                                 <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
                                     <span class="block sm:inline">{{ session('success') }}</span>
                                 </div>
@@ -90,7 +26,7 @@
                         @endif
                         @if (session('error'))
                         <div class="pt-3">
-                            <div class="max-w-7xl mx-auto sm:px-1 lg:px-2 xl:px-0">
+                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 xl:px-0">
                                 <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                                     <span class="block sm:inline">{{ session('error') }}</span>
                                 </div>
@@ -98,15 +34,12 @@
                         </div>
                         @endif
 
-                    <!-- make label and button in line -->
-                    <div class="p-1 text-gray-900">
-                    <label class="pr-2">Register New User</label>
+                    
                     <button
                         onclick="window.location.href='{{ route('register') }}'"
                         class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-md mt-6">
                         Register
                     </button>
-                    </div>
                     
 
                     
@@ -114,12 +47,11 @@
             </div>
         </div>
     </div>
-
-    <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-1 lg:px-2">
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-1 text-gray-900 p-2" >
-                    
+                <div class="p-6 text-gray-900 p-4" >
+                    <!-- {{ __("Welcome! ") }} -->
                     <form action="{{ route('set-admin') }}" method="POST">
                         @csrf
                         Choose User
@@ -140,11 +72,10 @@
             </div>
         </div>
     </div>
-
-    <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-1 lg:px-2">
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-gray-900">
+                <div class="p-6 text-gray-900">
                     <!-- {{ __("Welcome! ") }} -->
                     <form action="{{ route('set-verifier') }}" method="POST">
                         @csrf
@@ -166,12 +97,11 @@
             </div>
         </div>
     </div>
-    <!-- end of set verifier -->
-    <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-1 lg:px-2">
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-gray-900 flex flex-row">
-                    <div class="px-1">
+                <div class="p-6 text-gray-900 flex flex-row">
+                    <div class="px-3">
 
                         <form action="{{ route('assign-passports') }}" method="POST" class="flex items-center gap-4">
                             @csrf
@@ -198,17 +128,11 @@
             </div>
         </div>
     </div>
-    <!-- end of assign passports -->
-    
-    
-    </div>
 
-    <div class="shrink-0 border-t px-2 py-2 sm:px-2 lg:w-96 lg:border-l lg:border-t-0 lg:pr-3 xl:pr-3">
-      <!-- Right column area -->
-      <div class="pt-1">
-        <div class="max-w-7xl mx-auto sm:px-1 lg:px-2">
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-2 text-gray-900 flex flex-col">
+                <div class="p-6 text-gray-900 flex flex-col">
                     <div class="px-3">
 
                         <form action="{{ route('assign-users') }}" method="POST" class="flex flex-col items-left gap-4">
@@ -255,12 +179,43 @@
             </div>
         </div>
     </div>
-    <!-- end of assign users -->
-      
-    </div>
-  </div>
-</div>
 
+    <div class="pt-3">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 flex flex-col">
+                    <div class="px-3">
+                        <!-- Passport per User Count -->
+                        @php
+                            $passportsPerUser = $passports->count() / $users->count();
+                        @endphp 
+                        <table class="border border-slate-500">
+                            <th class="border border-slate-500 text-left p-2">User</th>
+                            <th class="border border-slate-500 text-center p-2">Assigned</th>
+                            <th class="border border-slate-500 text-center p-2">Entered</th>
+                        @foreach ($users as $user)
+                        <tr>
+                                <td class="border border-slate-500 p-2">
+                            
+                                    {{ $user->name }}
+                                </td>
+                                <td class="border border-slate-500 text-center p-2">
+                                    {{ $passports->where('user_id', $user->id)->count() }}
+                                </td>
+                                <td class="border border-slate-500 text-center p-2">
+                                    {{ $passports->where('user_id', $user->id)
+                                    ->where('is_data_entered', true)
+                                    ->count() }}
+                                
+                            </td>
+                        </tr>
+                        @endforeach
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+            </div>
         
     @endsection
 </x-app-layout>
