614aacfa1694295967e0b007ea14cf5344f448dd
mmkaw <mm.kawa@outlook.com>
2025-07-08 13:38:25 +0400

    added home link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,14 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-16 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link> 
+                    <x-nav-link :href="route('ppdashboard')" :active="request()->routeIs('ppdashboard')">
                         {{ __('Passport') }}
                     </x-nav-link>
                     <x-nav-link :href="route('leaveApplication')" :active="request()->routeIs('leaveApplication')">
@@ -99,8 +102,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                {{ __('home') }}
             </x-responsive-nav-link>
         </div>
         @if (Auth::user()->is_admin || Auth::user()->is_verifier)
