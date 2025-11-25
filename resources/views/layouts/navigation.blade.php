741aaeeac0c72549a52bc52b2266a3106a547b34
mmkaw <mm.kawa@outlook.com>
2025-11-25 17:57:10 +0400

    TIPL_4

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            @if(Auth::user()->name)
+                            @if(Auth::user())
                                 <div>{{ Auth::user()->name }}</div>
                             @endif
 
@@ -87,7 +87,7 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        @if(Auth::user()->name)
+        @if(Auth::user())
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
